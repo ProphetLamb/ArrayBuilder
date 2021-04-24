28abc7bb411185a7e06a327f9a0c2354a48ea9ba
ProphetLamb <rene.carannante@outlook.de>
2021-04-24 18:36:33 +0200

    Remove unused dependencies

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -1,10 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
-using System.Reflection;
-using System.Reflection.Metadata;
 
 using NUnit.Framework;
 
